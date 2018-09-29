439a28894f34c26b552c08a7e97bc3e3b7e24fa7
Bryan Tong <btong@berkeley.edu>
2018-09-29 14:43:19 -0700

    Initial Commit

---
+++ Keyboard Freezer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Keyboard Freezer
+//
+//  Created by Bryan Tong on 9/29/18.
+//  Copyright © 2018 btong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Keyboard Freezer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Keyboard Freezer
+//
+//  Created by Bryan Tong on 9/29/18.
+//  Copyright © 2018 btong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
