fd02b2b4b088473e724782f500665b4acaa898b0
Bryan Tong <btong@berkeley.edu>
2018-10-04 22:52:49 -0700

    Progress

--- Keyboard Freezer/ViewController.swift
+++ Keyboard Freezer/ViewController.swift
@@ -10,18 +10,73 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
+    
+    @IBOutlet var textField: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSEvent.addLocalMonitorForEvents(matching: .flagsChanged) {
+            self.flagsChanged(with: $0)
+            return $0
+        }
+        NSEvent.addLocalMonitorForEvents(matching: .keyDown) {
+            self.keyDown(with: $0)
+            return $0
+        }
+    }
+    override func keyDown(with event: NSEvent) {
+        switch event.modifierFlags.intersection(.deviceIndependentFlagsMask) {
+        case [.command] where event.characters == "l",
+             [.command, .shift] where event.characters == "l":
+            print("command-l or command-shift-l")
+        default:
+            break
+        }
+        textField.stringValue = "key = " + (event.charactersIgnoringModifiers
+            ?? "")
+        textField.stringValue += "\ncharacter = " + (event.characters ?? "")
+    }
+    override func flagsChanged(with event: NSEvent) {
+        switch event.modifierFlags.intersection(.deviceIndependentFlagsMask) {
+        case [.shift]:
+            print("shift key is pressed")
+        case [.control]:
+            print("control key is pressed")
+        case [.option] :
+            print("option key is pressed")
+        case [.command]:
+            print("Command key is pressed")
+        case [.control, .shift]:
+            print("control-shift keys are pressed")
+        case [.option, .shift]:
+            print("option-shift keys are pressed")
+        case [.command, .shift]:
+            print("command-shift keys are pressed")
+        case [.control, .option]:
+            print("control-option keys are pressed")
+        case [.control, .command]:
+            print("control-command keys are pressed")
+        case [.option, .command]:
+            print("option-command keys are pressed")
+        case [.shift, .control, .option]:
+            print("shift-control-option keys are pressed")
+        case [.shift, .control, .command]:
+            print("shift-control-command keys are pressed")
+        case [.control, .option, .command]:
+            print("control-option-command keys are pressed")
+        case [.shift, .command, .option]:
+            print("shift-command-option keys are pressed")
+        case [.shift, .control, .option, .command]:
+            print("shift-control-option-command keys are pressed")
+        default:
+            print("no modifier keys are pressed")
+        }
+    }
 
 }
 
